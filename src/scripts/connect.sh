6a052990e2387c87a75f43e187f2f4e194b3a98d
Yann <yann.domingo@gmail.com>
2022-04-27 17:55:49 +0100

    Dolla' dolla'

--- src/scripts/connect.sh
+++ src/scripts/connect.sh
@@ -27,11 +27,11 @@ EOF
     tailscale ping "$TS_DST_HOST"
     ;;
   linux)
-    sudo tailscale up --authkey="TS_KEY" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes
+    sudo tailscale up --authkey="$TS_KEY" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes
     tailscale ping "$TS_DST_HOST"
     ;;
   windows)
-    /c/PROGRA~2/"Tailscale IPN"/tailscale.exe up --authkey="TS_KEY" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes
+    /c/PROGRA~2/"Tailscale IPN"/tailscale.exe up --authkey="$TS_KEY" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes
     /c/PROGRA~2/"Tailscale IPN"/tailscale.exe ping "$TS_DST_HOST"
     ;;
 esac
