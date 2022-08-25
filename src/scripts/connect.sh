43fc0c9494928b6a5c57b25e9ae7be3bac827903
yaningo <yann.domingo@gmail.com>
2022-08-25 23:54:12 +0100

    Shell parameter expansion

--- src/scripts/connect.sh
+++ src/scripts/connect.sh
@@ -23,15 +23,15 @@ EOF
     sudo launchctl load /Library/LaunchDaemons/com.tailscale.tailscaled.plist
     sudo launchctl start com.tailscale.tailscaled
     
-    tailscale up --authkey "${<< parameters.ts-auth-key >>}" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes
+    tailscale up --authkey "${${!TS_AUTH_KEY}}" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes
     tailscale ping "$TS_DST_HOST"
     ;;
   linux)
-    sudo tailscale up --authkey="${<< parameters.ts-auth-key >>}" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes
+    sudo tailscale up --authkey="${!TS_AUTH_KEY}" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes
     tailscale ping "$TS_DST_HOST"
     ;;
   windows)
-    /c/PROGRA~2/"Tailscale IPN"/tailscale.exe up --authkey="${<< parameters.ts-auth-key >>}" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes
+    /c/PROGRA~2/"Tailscale IPN"/tailscale.exe up --authkey="${${!TS_AUTH_KEY}}" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes
     /c/PROGRA~2/"Tailscale IPN"/tailscale.exe ping "$TS_DST_HOST"
     ;;
 esac
