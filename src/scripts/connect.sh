e42b76ced04e28b52d456f45af72519986d209db
yaningo <yann.domingo@gmail.com>
2022-08-25 23:55:55 +0100

    Fixing syntax

--- src/scripts/connect.sh
+++ src/scripts/connect.sh
@@ -23,7 +23,7 @@ EOF
     sudo launchctl load /Library/LaunchDaemons/com.tailscale.tailscaled.plist
     sudo launchctl start com.tailscale.tailscaled
     
-    tailscale up --authkey "${${!TS_AUTH_KEY}}" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes
+    tailscale up --authkey "${!TS_AUTH_KEY}}" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes
     tailscale ping "$TS_DST_HOST"
     ;;
   linux)
@@ -31,7 +31,7 @@ EOF
     tailscale ping "$TS_DST_HOST"
     ;;
   windows)
-    /c/PROGRA~2/"Tailscale IPN"/tailscale.exe up --authkey="${${!TS_AUTH_KEY}}" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes
+    /c/PROGRA~2/"Tailscale IPN"/tailscale.exe up --authkey="${!TS_AUTH_KEY}}" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes
     /c/PROGRA~2/"Tailscale IPN"/tailscale.exe ping "$TS_DST_HOST"
     ;;
 esac
