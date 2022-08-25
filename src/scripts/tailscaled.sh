c854ca2f63df187f5724221747ba690ef6490a70
yaningo <yann.domingo@gmail.com>
2022-08-26 00:14:46 +0100

    Testing wrong env var for Tailscal auth key param

---
+++ src/scripts/tailscaled.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+printf "Executor is $EXECUTOR.\n"
+case $EXECUTOR in
+  linux|windows|macos)
+    echo " > Nothing to do"
+    ;;
+docker)
+echo " > Starting tailscaled"
+tailscaled --tun=userspace-networking --outbound-http-proxy-listen=localhost:1054 --socks5-server=localhost:1055 --socket=/tmp/tailscaled.sock 1>/dev/null 2>/tmp/tailscaled.log
+;;
