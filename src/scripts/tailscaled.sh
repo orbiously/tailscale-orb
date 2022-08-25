5b931a3bd25c104c255bb6e0705a6cd23adc19d1
yaningo <yann.domingo@gmail.com>
2022-08-26 00:17:02 +0100

    Fixing shell

--- src/scripts/tailscaled.sh
+++ src/scripts/tailscaled.sh
@@ -4,7 +4,8 @@ case $EXECUTOR in
   linux|windows|macos)
     echo " > Nothing to do"
     ;;
-docker)
-echo " > Starting tailscaled"
-tailscaled --tun=userspace-networking --outbound-http-proxy-listen=localhost:1054 --socks5-server=localhost:1055 --socket=/tmp/tailscaled.sock 1>/dev/null 2>/tmp/tailscaled.log
-;;
+  docker)
+    echo " > Starting tailscaled"
+    tailscaled --tun=userspace-networking --outbound-http-proxy-listen=localhost:1054 --socks5-server=localhost:1055 --socket=/tmp/tailscaled.sock 1>/dev/null 2>/tmp/tailscaled.log
+    ;;
+esac
