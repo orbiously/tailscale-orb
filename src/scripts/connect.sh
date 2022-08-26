fe0fa8805985502936b91eade2bb7a0fc91ae058
Yann <yann.domingo@gmail.com>
2022-08-26 11:53:13 +0100

    only the =

--- src/scripts/connect.sh
+++ src/scripts/connect.sh
@@ -4,9 +4,9 @@ case $EXECUTOR in
   docker)
     tailscale --socket=/tmp/tailscaled.sock up --authkey="${!TS_AUTH_KEY}" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes
 
-    remote_tailscale_host_check=("tailscale --socket=/tmp/tailscaled.sock status")                
+    remote_tailscale_host_check=(tailscale --socket'='/tmp/tailscaled.sock status)                
     
-    ping_remote_tailscale_host=("tailscale --socket=/tmp/tailscaled.sock")
+    ping_remote_tailscale_host=(tailscale --socket'='/tmp/tailscaled.sock)
     ;;
   macos)
 cat << EOF | sudo tee /Library/LaunchDaemons/com.tailscale.tailscaled.plist 1>/dev/null
