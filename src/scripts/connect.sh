bdbbefa372df032baabe294769611350dc985733
Yann <yann.domingo@gmail.com>
2022-08-27 18:55:24 +0100

    Refactoring and invalid key handling

--- src/scripts/connect.sh
+++ src/scripts/connect.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 if [ -z "${!PARAM_TS_AUTH_KEY}" ]; then 
-    printf "The environment variable you specified for the Tailscale authentication key (%s) is not set.\n" ${PARAM_TS_AUTH_KEY}
-    echo "- Did you declare an environment variable that contains the Tailscale authentication key? If so, is ${PARAM_TS_AUTH_KEY} the correct name?"
-    echo "- Did you declare the environment variable in an organization context? If so, did you specify the context name in the workflow?"
+    printf "The environment variable that is supposed to contain the Tailscale authentication key is not set.\n"
+    echo "- Did you store the Tailscale authentication key in an evironment variable named ${PARAM_TS_AUTH_KEY}?"
+    echo "- In case you stored the Tailscale authentication key in an environment variable with different name than TS_AUTH_KEY, did you specify that custom name via the ts-auth-key parameter?"
+    echo "- Is the environment variable delared in an organization context? If so, did you specify the context name in the workflow?"
     exit 1
 fi
 
@@ -12,7 +13,9 @@ case $EXECUTOR in
   docker)
     tmux new-session -d -s "TempSession" tailscaled --tun=userspace-networking --outbound-http-proxy-listen=localhost:1054 --socks5-server=localhost:1055 --socket=/tmp/tailscaled.sock 1>/dev/null 2>/tmp/tailscaled.log
 
-    tailscale --socket=/tmp/tailscaled.sock up --authkey="${!PARAM_TS_AUTH_KEY}" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes
+    #tailscale --socket=/tmp/tailscaled.sock up --authkey="${!PARAM_TS_AUTH_KEY}" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes); then
+
+    tailscale_connect=("tailscale" "--socket=/tmp/tailscaled.sock" "up" "--authkey=${!PARAM_TS_AUTH_KEY}" "--hostname=$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" "--accept-routes")  
 
     tailscale_status=("tailscale" "--socket=/tmp/tailscaled.sock" "status")                
     
@@ -41,26 +44,31 @@ EOF
     sudo launchctl load /Library/LaunchDaemons/com.tailscale.tailscaled.plist
     sudo launchctl start com.tailscale.tailscaled
     
-    tailscale up --authkey "${!PARAM_TS_AUTH_KEY}" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes
+    #tailscale up --authkey "${!PARAM_TS_AUTH_KEY}" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes
+    tailscale_connect=("tailscale up" "--authkey=${!PARAM_TS_AUTH_KEY}" "--hostname=$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" "--accept-routes")
     tailscale_status=(tailscale status)
     tailscale_ping=(tailscale ping)
     ;;
   linux)
-    sudo tailscale up --authkey="${!PARAM_TS_AUTH_KEY}" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes
+    #sudo tailscale up --authkey="${!PARAM_TS_AUTH_KEY}" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes
+    tailscale_connect=("sudo" "tailscale up" "--authkey=${!PARAM_TS_AUTH_KEY}" "--hostname=$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" "--accept-routes")
     tailscale_status=(tailscale status)
     tailscale_ping=(tailscale ping)
     ;;
   windows)
     /c/PROGRA~2/"Tailscale IPN"/tailscale.exe up --authkey="${!PARAM_TS_AUTH_KEY}" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes
+    tailscale_connect=("/c/PROGRA~2/Tailscale IPN/tailscale.exe up" "--authkey=${!PARAM_TS_AUTH_KEY}" --hostname="$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes)
     tailscale_status=(/c/PROGRA~2/"Tailscale IPN"/tailscale.exe status)
     tailscale_ping=(/c/PROGRA~2/"Tailscale IPN"/tailscale.exe ping)
     ;;
 esac
 
+if (! "${tailscale_connect[@]}"); then
+  printf "Either:\n - The Tailscale auth key you're using is invalid\n or\n - The \"Device Authorization > Manually authorize new devices\" Tailnet setting is enabled" and the Tailscale auth key is NOT pre-authorized (https://tailscale.com/kb/1099/device-authorization/)"
+
 if ( "${tailscale_status[@]}"  | grep jumper | grep "offline" ); then
   printf "\nRemote Tailscale host is offline\n"
   printf "\nMake sure Tailscale is started on the remote host before attempting to run this job again\n"
-  printf "\nFailing the build\n"
   exit 1
 fi
 
