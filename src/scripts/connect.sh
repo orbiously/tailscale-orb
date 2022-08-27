be5415ca2947b46edd8153eef1b7735d744ac916
Yann <yann.domingo@gmail.com>
2022-08-27 20:16:38 +0100

    DRY macOS (should fail: invalid key)

--- src/scripts/connect.sh
+++ src/scripts/connect.sh
@@ -52,7 +52,7 @@ EOF
     #   exit 1
     # fi
     
-    tailscale_connect=("tailscale up --authkey=${!PARAM_TS_AUTH_KEY} --hostname=$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM --accept-routes")
+    tailscale_connect=(tailscale up "--authkey=${!PARAM_TS_AUTH_KEY}" "--hostname=$CIRCLE_PROJECT_USERNAME-$CIRCLE_PROJECT_REPONAME-$CIRCLE_BUILD_NUM" --accept-routes)
 
     tailscale_status=(tailscale status)
     tailscale_ping=(tailscale ping)
